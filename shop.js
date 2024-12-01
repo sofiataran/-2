1f0c02668f8bf211c64070c18fd5c8c9781d9d28
Sofia Taran <sonataran0@gmail.com>
2024-12-01 21:00:35 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -13,7 +13,7 @@ function sort(){
 
 async function getResponce() {
 
-    let responce = await fetch("shop.json")
+    let responce = await fetch("./shop.json")
 
     let content = await responce.text()
     console.log(content)
@@ -50,7 +50,7 @@ async function getResponce1() {
     //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
     //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
 //    let responce = await fetch("shop.json")
-    let responce = await fetch("shop.json")
+    let responce = await fetch("./shop.json")
 
     let content = await responce.text()
     console.log(content)
